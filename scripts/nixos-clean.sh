d22f8f7f18c3bea26420cc599d4dc51359501aec
momo <luna.cloudberry@gmail.com>
2025-09-04 05:07:15 +0800

    Remove nixos GC scripts
    We prefer the tool `nh` and the command `nh clean all --ask --verbose`
    instead for this kind of job.

--- scripts/nixos-clean.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-echo "This script remove profiles since n days ago"
-
-# $1 can be 14d for 14 days
-nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than "$1"
