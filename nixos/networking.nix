5dce12d349950efceace5c795195cdd5db0a5a88
momo <luna.cloudberry@gmail.com>
2026-01-09 14:25:30 +0800

    Remove broken public DNS resolver
    It resolves DNS but bricks my PC date and time systems.
    Also breaks nixos-rebuild for some reason. I suspect
    because it depends on functioning datetime system.

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -15,29 +15,8 @@ inputs@{ credentials, ... }:
   networking = {
     networkmanager = {
       enable = true;
-      settings = {
-        connection = {
-          # If unspecified, the ultimate default values depends on the
-          # DNS plugin. With systemd-resolved the default currently is
-          # its global setting and for all other plugins "no" (0).
-          #
-          # Set this to 0 for services.resolved to work properly.
-          dns-over-tls = 0;
-        };
-      };
     };
     hostName = credentials.host.name;
-  };
-  services.resolved = {
-    enable = true;
-    # Always encrypts DNS lookups.
-    # WARNING: Set this to "false" if we want to access captive
-    # portals as there may be issues.
-    dnsovertls = "true";
-    domains = [ "~." ];
-    extraConfig = ''
-      DNS=1.1.1.1#one.one.one.one 1.0.0.1#one.one.one.one 2606:4700:4700::1111#one.one.one.one 2606:4700:4700::1001#one.one.one.one
-    '';
   };
 
   # Some programs need SUID wrappers, can be configured further or are
