fd73d2b1474bf08b2350458557606257d4befcf1
momo <luna.cloudberry@gmail.com>
2026-02-02 15:58:11 +0800

    Update Haskell dev code

--- developments/haskell/README.md
+++ developments/haskell/README.md
@@ -1,7 +1,1 @@
-Summoner config file isn't updated for GHC 9.10 series.
-
-Make sure after summoned:
-
-- Change default-language to GHC2024
-- Tested with: == ghc9.10.1
-- base >= 4.20
+Example template for GHC 9.14.1 project.
--- developments/haskell/flake.nix
+++ developments/haskell/flake.nix
@@ -1,40 +1,50 @@
 {
-  description = "A top down game";
+  description = "";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
       in
-        {
-          devShells.default = with pkgs; pkgs.mkShell rec {
+      {
+        devShells.default =
+          with pkgs;
+          pkgs.mkShell rec {
             nativeBuildInputs = [
-              haskell.compiler.ghc914
-              haskellPackages.cabal-install
-              
+              upx
+
               ghciwatch
               haskellPackages.fourmolu
               haskellPackages.cabal-gild
-              
+              haskellPackages.hlint
+
               sqlite
             ];
             buildInputs = [
+              haskell.compiler.ghc914
+              haskellPackages.cabal-install
               pkg-config
-              upx
               zlib
+              gmp
             ];
 
             LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
-          
-        };
+
+          };
       }
     );
 }
-
