699171b7cff58789f4c350b4407330df31aee99a
momo <luna.cloudberry@gmail.com>
2026-04-17 20:48:53 +0800

    chore: add trusted users

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,6 +88,8 @@
     "flakes"
   ];
 
+  nix.settings.trusted-users = [ "root" "momo" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
