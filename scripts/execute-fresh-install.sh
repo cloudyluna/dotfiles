6678d9b2b3813f963b0a238d9d2e74c0f9a6fbb2
momo <luna.cloudberry@gmail.com>
2026-07-06 21:06:00 +0800

    exit properly when a command fails in install script

--- scripts/execute-fresh-install.sh
+++ scripts/execute-fresh-install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-cp -f ./nixos/disko-config.nix /tmp
-nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /tmp/disko-config.nix
-nixos-generate-config --no-filesystems --root /mnt
+cp -f ./nixos/disko-config.nix /tmp || exit 1
+nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /tmp/disko-config.nix || exit 1
+nixos-generate-config --no-filesystems --root /mnt || exit 1
 
-cp -r ./nixos/* /mnt/etc/nixos/
+cp -r ./nixos/* /mnt/etc/nixos/ || exit 1
 
-nixos-install --flake /mnt/etc/nixos#nixos
+nixos-install --flake /mnt/etc/nixos#nixos || exit 1
 
